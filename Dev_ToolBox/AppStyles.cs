60950fd2af490240fea4b1f616d37a834fe80425
ThomasPiessat <42283037+ThomasPiessat@users.noreply.github.com>
2024-03-07 19:00:55 +0000

    Start styling

---
+++ Dev_ToolBox/AppStyles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Dev_ToolBox
+{
+    public static class AppStyles
+    {
+        public static SolidColorBrush SidebarBackgroundColor { get; } = new SolidColorBrush(Color.FromRgb(44, 62, 80));
+        public static SolidColorBrush ButtonForegroundColor { get; } = Brushes.White;
+        // Add more style properties as needed
+    }
+}

156e9072536e7d96f77e35abc24cefb3053e0e46
ThomasPiessat <42283037+ThomasPiessat@users.noreply.github.com>
2024-03-17 14:16:45 +0000

    Add DiffPlex lib + start DiffViewerPage

--- Dev_ToolBox/DiffViewerPage.xaml.cs
+++ Dev_ToolBox/DiffViewerPage.xaml.cs
@@ -10,8 +10,12 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DiffPlex;
+using DiffPlex.DiffBuilder;
+using DiffPlex.DiffBuilder.Model;
 
 namespace Dev_ToolBox
 {
@@ -27,27 +31,65 @@ namespace Dev_ToolBox
 
         private void CompareButton_Click(object sender, RoutedEventArgs e)
         {
-            string leftText = txtLeftText.Text;
-            string rightText = txtRightText.Text;
+            string InputText = txtInput.Text;
+            string OutputText = txtOutput.Text;
 
-            string[] leftLines = leftText.Split('\n');
-            string[] rightLines = rightText.Split('\n');
+            SideBySideDiffModel diffModel = GenerateDiff(InputText, OutputText);
 
-            StringBuilder diffResult = new StringBuilder();
+            //Display the differences
+            DisplayDiff(diffModel);
+        }
 
-            int minLength = Math.Min(leftLines.Length, rightLines.Length);
+        private SideBySideDiffModel GenerateDiff(string inputText, string outputText)
+        {
+            var differ = new Differ();
+            var builder = new SideBySideDiffBuilder(differ);
+            var diffModel = builder.BuildDiffModel(inputText, outputText);
 
-            for (int i = 0; i < minLength; i++)
+            return diffModel;
+        }
+
+        private void DisplayDiff(SideBySideDiffModel diffModel)
+        {
+            txtInput.Clear();
+            txtOutput.Clear();
+
+            foreach (var line in diffModel.OldText.Lines)
             {
-                if (leftLines[i] != rightLines[i])
-                {
-                    diffResult.AppendLine($"Line {i + 1}:");
-                    diffResult.AppendLine($"- {leftLines[i].Trim()}");
-                    diffResult.AppendLine($"+ {rightLines[i].Trim()}");
-                }
-            }
+                //    if(line.Type == ChangeType.Deleted)
+                //    {
+                //        txtInput.AppendText(line.Text + "\r\n");
+                //        txtInput.Background = System.Windows.Media.Brushes.LightSalmon;
+                //    }
+                //    txtInput.AppendText(line.Text + "\r\n");
+                //}
 
-            txtDiffResult.Text = diffResult.ToString();
+                //foreach(var line in diffModel.NewText.Lines)
+                //{
+                //    if( line.Type == ChangeType.Inserted) 
+                //    {
+                //        txtOutput.AppendText(line.Text + "\r\n");
+                //        txtOutput.Background = System.Windows.Media.Brushes.LightGreen;
+                //    }
+                //    txtOutput.AppendText(line.Text + "\r\n");
+                //}
+                if (line.Type == ChangeType.Deleted)
+                {
+                    txtInput.AppendText(line.Text + "\r\n");
+                    txtInput.Background = System.Windows.Media.Brushes.LightSalmon;
+                }
+                else if (line.Type == ChangeType.Inserted)
+                {
+                    txtOutput.AppendText(line.Text + "\r\n");
+                    txtOutput.Background = System.Windows.Media.Brushes.LightGreen;
+                }
+                else
+                {
+                    txtInput.AppendText(line.Text + "\r\n");
+                    txtOutput.AppendText(line.Text + "\r\n");
+                }
+
+            }
         }
     }
 }
--- Dev_ToolBox/obj/Release/net8.0-windows10.0.19041.0/Dev_ToolBox.AssemblyInfo.cs
+++ Dev_ToolBox/obj/Release/net8.0-windows10.0.19041.0/Dev_ToolBox.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dev_ToolBox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75f4db902634d05b88033512cec4ecd0cd235134")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c0a4c5314934bfe886efe6b591924e042a0b57e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dev_ToolBox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dev_ToolBox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

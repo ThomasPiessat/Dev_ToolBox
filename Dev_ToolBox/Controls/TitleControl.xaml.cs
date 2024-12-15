35e3d42cf9a0c4aa35ebf497bbab69e32904dc74
ThomasPiessat <42283037+ThomasPiessat@users.noreply.github.com>
2024-12-15 18:26:23 +0100

    Add Title Control to uniform all titles

---
+++ Dev_ToolBox/Controls/TitleControl.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Dev_ToolBox.Controls
+{
+    /// <summary>
+    /// Interaction logic for TitleControl.xaml
+    /// </summary>
+    public partial class TitleControl : UserControl
+    {
+        public string ToolName
+        {
+            get { return txtTitle.Text; }
+            set { txtTitle.Text = value; }
+        }
+
+        public TitleControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
